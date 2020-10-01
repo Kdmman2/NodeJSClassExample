a5ad645eac26965d1f2490e57bbccb39ff7fc52d
Kdmman2 <kdmman2@gmail.com>
2020-10-01 23:04:39 +0000

    MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 //require body-parser
 var bodyParser = require("body-parser");
 //create express object, call express
@@ -11,6 +12,14 @@ app.use(express.static('public'));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//Connection information for Mongo
+const Todo = require('./models/todo.model');
+const mongoDB = 'mongodb+srv://testConnection:b8RwqJYgo4hD1xhe@nodetodoexample-iqnde.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '))
 //Couple of items todo
 var tasks = ["Procrastinate", "Do homework last minute"];
 
@@ -19,8 +28,24 @@ var completed = ["extra work"];
 
 //get home page /
 app.get('/', function(req, res){
+
+    Todo.find(function(err, todo){
+        if(err){
+            console.log(err);
+        }
+        else{
+            for(i=0; i<todo.length; i++){
+                if(todo[i].done){
+                    completed.push(todo[i].item);
+                }
+                else{
+                    tasks.push(todo[i].item);
+                }
+            }
+        }
+    });
     //return something to home page
-    res.render('index', {tasks: tasks}); //add completed variable to ejs ex {a:a, b:b}
+    res.render('index', {tasks: tasks, completed: completed}); //add completed variable to ejs ex {a:a, b:b}
 });
 
 //add post method /addtask
